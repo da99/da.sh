a7ca8c23ca1b62e90de075d1c162d2de2505f507
da99 <1551848+da99@users.noreply.github.com>
2023-11-02 10:14:41 -0500

    Added: Alegria-grill slides

--- bin/da.sh
+++ bin/da.sh
@@ -254,7 +254,7 @@ case "$(echo "$@" | xargs)" in
     ;;
 
   mobile-repos)
-    for x in alegria da.sh my_uni my_dev01 my_bdrm jaki.club ; do
+    for x in alegria slides da.sh my_uni my_dev01 my_bdrm jaki.club ; do
       if test -e "/apps/$x" ; then
         echo "/apps/$x"
       fi
