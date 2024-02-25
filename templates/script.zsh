d8ac88a9cefa1aa9ea5e7e9b708780769cad90d7
da99 <1551848+da99@users.noreply.github.com>
2024-02-25 08:55:05 -0600

    Development checkpoint.

--- templates/script.zsh
+++ templates/script.zsh
@@ -3,5 +3,14 @@
 #
 set -u -e -o pipefail
 
+case "$*" in
+  "-h"|"--help"|"help")
+    echo "$0 -h|--help|help -- Show this message."
+    echo
+    ;;
+  *)
+    echo "!!! Unknown command: $*" >&2
+    exit 1
+    ;;
+esac
 
-echo "--- Not implemented: $0" >&2
