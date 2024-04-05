53a30d4436103183c075dbfca34bf9285ed28ab1
da99 <1551848+da99@users.noreply.github.com>
2024-04-05 05:33:31 -0500

    Touchups.

--- templates/public_file.sql
+++ templates/public_file.sql
@@ -1,11 +1,16 @@
 
-
 CREATE TABLE IF NOT EXISTS `files` (
-  `version_path` STRING NOT NULL PRIMARY KEY,
+  `public_path`  STRING NOT NULL PRIMARY KEY,
   `local_path`   STRING NOT NULL,
-  `date_created` INTEGER CHECK (date_created > 1712272096),
-  `status`       INTEGER DEFAULT 0 CHECK (status < 4 AND status > -1)
+  `etag`         STRING CHECK (len(etag) > 5),
+  `created_at`   INTEGER CHECK (date_created > 1712272096),
+  `status`       INTEGER DEFAULT 0 CHECK (status < 3 AND status > -1)
 );
+
+-- STATUS:
+--   0: NOT UPLOADED
+--   1: UPLOADED
+--   2: PRUNED/DELETED
 
 -- SPLIT --
 
@@ -13,7 +18,3 @@ CREATE INDEX IF NOT EXISTS idx_local_path
   ON `files` (local_path, version_path);
 
 
--- STATUS:
---   0: NOT UPLOADED
---   1: UPLOADED
---   2: PRUNED/DELETED
